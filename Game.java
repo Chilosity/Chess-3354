de0001511d9c390ff578eec539394ff5cf38aba4
Chilosity <139659394+Chilosity@users.noreply.github.com>
2024-05-02 22:27:03 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 // Import custom classes for the game components
 import Pieces.*;
-import Player;
-import Board;
-import ChessGUI;
+import Player.;
+import Board.;
+import ChessGUI.;
 
 // Define the public class Game
 public class Game
